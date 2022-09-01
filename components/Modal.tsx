bddc321179d9ffaa3b8f08e92dd83db88d3cbe95
Yasiel Hernández Portal <yasielhp@Workstation-MacBook.local>
2022-09-01 13:01:03 +0100

    change color text modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -10,7 +10,7 @@ export const Modal = ({ text, onClick }: Props) => {
 	return (
 		<div className="flex bg-opacity-60 backdrop-blur-xl justify-center items-center w-screen h-full">
 			<div className="z-10 absolute top-10 flex flex-col justify-center items-center bg-white drop-shadow-xl p-5 rounded-md">
-				<p className="mb-4 w-72 text-center text-mirage">{text}</p>
+				<p className="mb-4 w-72 text-center text-stone-800">{text}</p>
 				<NavButton isActive onClick={onClick} title="Login with Metamask" />
 			</div>
 		</div>
